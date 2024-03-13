9dfdc2acf667e8b19a338ad4bd4401a4740ecd5d
Marcelo Junior <marcelo.junior@yes.technology>
2024-03-13 08:44:51 -0300

    fix: get json

--- src/server.js
+++ src/server.js
@@ -1,17 +1,33 @@
 import get from './return/get.json' assert { type: "json" }
 import post from './return/post.json' assert { type: "json" }
+import put from './return/put.json' assert { type: "json" }
+
+import dotenv from 'dotenv'
+dotenv.config();
 
 import express from 'express';
 
 const app = express();
-const port = 8888
+const port = process.env.PORT || 8888 ;
+const getStatusCode = process.env.GET_STATUS_CODE || 200 ;
+const postStatusCode = process.env.POST_STATUS_CODE || 200 ;
+const putStatusCode = process.env.PUT_STATUS_CODE || 200 ;
+
+
 
 app.get("/*", (req, res) => {
-    res.json(get)
+    console.log('GET: ', req.url);
+    res.status(200).json(get || "")
 });
 
 app.post("/*", (req, res) => {
-    res.json(post)
+    console.log('POST: ', req.url);
+    res.status(200).json(post || "")
+});
+
+app.put ("/*", (req, res) => {
+    console.log('PUT: ', req.url);
+    res.status(200).send(put || "")
 });
 
 app.listen(port);
